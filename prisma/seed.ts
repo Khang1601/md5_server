d26e008ab01ba679bb43a71dc9809b9449fa4806
Khang1601 <dinhkhang1601@gmail.com>
2024-02-26 22:08:39 +0700

    1

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -23,14 +23,14 @@ async function main() {
         const categoriesData = [
             { title: "Shoes", avatar: "https://png.pngtree.com/png-clipart/20201209/original/pngtree-shoes-icon-design-template-illustration-png-image_5679703.jpg" },
             { title: "Bags", avatar: "https://w7.pngwing.com/pngs/568/582/png-transparent-shopping-bags-trolleys-bag-accessories-sticker-shopping-bags-trolleys-thumbnail.png" },
-            { title: "Hats", avatar: "https://png.pngtree.com/element_our/20190531/ourmid/pngtree-free-cartoon-black-hat-image_1321651.jpg" }
+            { title: "Hats" , avatar: "https://png.pngtree.com/element_our/20190531/ourmid/pngtree-free-cartoon-black-hat-image_1321651.jpg" }
           ];
           
           for (const category of categoriesData) {
             await prisma.categories.create({ data: category });
           }
         
-    }catch(err) {
+    }catch(err) {   
 
     }
 }
